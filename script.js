429f8517f53b689c681632138f86fccfe135feeb
John <johnfoong03@gmail.com>
2023-08-30 13:32:43 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,317 @@
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+let b4 = document.getElementById("b4");
+let b10 = document.getElementById("b10");
+let b11 = document.getElementById("b11");
+let b20 = document.getElementById("b20");
+let b21 = document.getElementById("b21");
+let b30 = document.getElementById("b30");
+let b31 = document.getElementById("b31");
+let b40 = document.getElementById("b40");
+let b41 = document.getElementById("b41");
+
+b1.onclick = function () {
+  if ((b10.style.display = "block")) {
+    b11.style.display = "block";
+
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+  }
+};
+
+b2.onclick = function () {
+  if ((b20.style.display = "block")) {
+    b21.style.display = "block";
+
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+  }
+};
+b3.onclick = function () {
+  if ((b30.style.display = "block")) {
+    b31.style.display = "block";
+
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+  }
+};
+b4.onclick = function () {
+  if ((b40.style.display = "block")) {
+    b41.style.display = "block";
+
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+  }
+};
+
+let one = document.getElementById("one");
+let ones = document.getElementById("ones");
+b10.onclick = function () {
+  if ((one.style.display = "block")) {
+    ones.style.display = "none";
+
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+  }
+};
+
+b11.onclick = function () {
+  if ((ones.style.display = "block")) {
+    one.style.display = "none";
+
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+  }
+};
+let two = document.getElementById("two");
+let twos = document.getElementById("twos");
+b20.onclick = function () {
+  if ((two.style.display = "block")) {
+    twos.style.display = "none";
+
+    one.style.display = "none";
+    ones.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    8;
+    four.style.display = "none";
+    8;
+    fours.style.display = "none";
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+  }
+};
+
+b21.onclick = function () {
+  if ((twos.style.display = "block")) {
+    two.style.display = "none";
+
+    one.style.display = "none";
+    ones.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+  }
+};
+let three = document.getElementById("three");
+let threes = document.getElementById("threes");
+b30.onclick = function () {
+  if ((three.style.display = "block")) {
+    threes.style.display = "none";
+
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+  }
+};
+
+b31.onclick = function () {
+  if ((threes.style.display = "block")) {
+    three.style.display = "none";
+
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    four.style.display = "none";
+    fours.style.display = "none";
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b40.style.display = "none";
+    b41.style.display = "none";
+  }
+};
+let four = document.getElementById("four");
+let fours = document.getElementById("fours");
+b40.onclick = function () {
+  if ((four.style.display = "block")) {
+    fours.style.display = "none";
+
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+  }
+};
+
+b41.onclick = function () {
+  if ((fours.style.display = "block")) {
+    four.style.display = "none";
+
+    one.style.display = "none";
+    ones.style.display = "none";
+    two.style.display = "none";
+    twos.style.display = "none";
+    three.style.display = "none";
+    threes.style.display = "none";
+    b10.style.display = "none";
+    b11.style.display = "none";
+    b20.style.display = "none";
+    b21.style.display = "none";
+    b30.style.display = "none";
+    b31.style.display = "none";
+  }
+};
+
+let kg = document.getElementById("kg");
+let kg1 = document.getElementById("kg1");
+let btn1 = document.getElementById("btn1");
+let share1 = document.getElementById("share1");
+let btn10 = document.getElementById("btn10");
+let share10 = document.getElementById("share10");
+
+btn1.onclick = function () {
+  let number = parseFloat(kg.value) * 2.205;
+  share1.innerText = number + " Pounds";
+};
+btn10.onclick = function () {
+  let number = parseFloat(kg1.value) / 2.205;
+  share10.innerText = number + " KG";
+};
+
+let meters = document.getElementById("meters");
+let meterss = document.getElementById("meterss");
+let btn2 = document.getElementById("btn2");
+let btn20 = document.getElementById("btn20");
+let share2 = document.getElementById("share2");
+let share20 = document.getElementById("share20");
+
+btn2.onclick = function () {
+  let number = parseFloat(meters.value) * 3.281;
+  share2.innerText = number + " Feet";
+};
+btn20.onclick = function () {
+  let number = parseFloat(meterss.value) / 3.281;
+  share20.innerText = number + " Meters";
+};
+
+let Celsius = document.getElementById("Celsius");
+let btn3 = document.getElementById("btn3");
+let share3 = document.getElementById("share3");
+let Celsiuss = document.getElementById("Celsiuss");
+let btn30 = document.getElementById("btn30");
+let share30 = document.getElementById("share30");
+
+btn3.onclick = function () {
+  let number = (parseFloat(Celsius.value) * 9) / 5 + 32;
+  share3.innerText = number + "°F";
+};
+btn30.onclick = function () {
+  let number = ((parseFloat(Celsiuss.value) - 32) * 5) / 9;
+  share30.innerText = number + "°C";
+};
+
+let time = document.getElementById("time");
+let btn4 = document.getElementById("btn4");
+let share4 = document.getElementById("share4");
+let times = document.getElementById("times");
+let btn40 = document.getElementById("btn40");
+let share40 = document.getElementById("share40");
+
+btn4.onclick = function () {
+  let number = parseFloat(time.value) / 60;
+  share4.innerText = number + " Minutes";
+};
+btn40.onclick = function () {
+  let number = parseFloat(times.value) * 60;
+  share40.innerText = number + " Seconds";
+};
